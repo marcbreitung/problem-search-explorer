c957483c85da63e091bc3e8cb34678bd865b708b
Marc Breitung <info@marcbreitung.de>
2018-03-05 17:48:09 +0100

    update node_modules modified new ProblemSearch.Problem() call

--- src/components/Explorer.vue
+++ src/components/Explorer.vue
@@ -73,7 +73,7 @@
                 this.searchStrategy = this.searchStrategyFactory.getStrategy(value.searchStrategy, value.strategyOptions)
                 graph = new ProblemSearch.Graph()
                 graph.addNodes(this.map.getNodes())
-                problem = new ProblemSearch.Problem(graph, new ProblemSearch.State(this.start.id, this.start), new ProblemSearch.State(this.goal.id, this.goal))
+                problem = new ProblemSearch.Problem(graph, new ProblemSearch.State(this.start), new ProblemSearch.State(this.goal))
                 try {
                     result = this.searchStrategy.search(problem)
                     this.$eventHub.$emit('solution', result.solutionGraph())
