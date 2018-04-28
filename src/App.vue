62431711ab81f303a515822c37bc4635a65d5e47
Marc Breitung <info@marcbreitung.de>
2018-04-28 13:48:35 +0200

    Update external dependencies (google fonts / font awesome)

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,18 @@
 </script>
 
 <style>
+    @font-face {
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 400;
+        src: url('assets/fonts/Roboto/roboto-v18-latin-regular.eot'); /* IE9 Compat Modes */
+        src: local('Roboto'), local('Roboto-Regular'),
+        url('assets/fonts/Roboto/roboto-v18-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+        url('assets/fonts/Roboto/roboto-v18-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+        url('assets/fonts/Roboto/roboto-v18-latin-regular.woff') format('woff'), /* Modern Browsers */
+        url('assets/fonts/Roboto/roboto-v18-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+        url('assets/fonts/Roboto/roboto-v18-latin-regular.svg#Roboto') format('svg'); /* Legacy iOS */
+    }
     * {
         box-sizing: border-box;
     }
